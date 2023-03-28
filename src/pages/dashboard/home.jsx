5b118b9aad9f2c0e958c4dbc1488a52a09617d27
ahab-latif-qbatch <ahab.latif@qbatch.com>
2023-03-29 00:08:24 +0500

    Tornument Cards added

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -80,57 +80,31 @@ export function Home() {
         {projectsData.map(
           ({ img, title, description, tag, route, members }) => (
             <Card key={title} color="transparent" shadow={false}>
-              <CardHeader
-                floated={false}
-                color="gray"
-                className="mx-0 mt-0 mb-4 h-64 xl:h-40"
-              >
-                <img
-                  src={img}
-                  alt={title}
-                  className="h-full w-full object-cover"
-                />
-              </CardHeader>
               <CardBody className="py-0 px-1">
                 <Typography
                   variant="small"
                   className="font-normal text-blue-gray-500"
                 >
-                  {tag}
+                  Tournament # 1
                 </Typography>
                 <Typography
                   variant="h5"
                   color="blue-gray"
                   className="mt-1 mb-2"
                 >
-                  {title}
+                  Al-KAbir Punjab Karate League 
                 </Typography>
                 <Typography
                   variant="small"
                   className="font-normal text-blue-gray-500"
                 >
-                  {description}
+                  25-03-2023 | Punjab
                 </Typography>
               </CardBody>
               <CardFooter className="mt-6 flex items-center justify-between py-0 px-1">
                   <Button variant="outlined" size="sm">
-                    view project
+                    Results
                   </Button>
-                <div>
-                  {members.map(({ img, name }, key) => (
-                    <Tooltip key={name} content={name}>
-                      <Avatar
-                        src={img}
-                        alt={name}
-                        size="xs"
-                        variant="circular"
-                        className={`cursor-pointer border-2 border-white ${
-                          key === 0 ? "" : "-ml-2.5"
-                        }`}
-                      />
-                    </Tooltip>
-                  ))}
-                </div>
               </CardFooter>
             </Card>
           )
